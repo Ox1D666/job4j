8e4d60682e27cd0beee980f74ff99c9ff7ba95bb
Kirill Romanov <kirill.coldline@gmail.com>
2019-12-04 16:09:24 +0300

    9.2. Тесты вывода на консоль в StartUI[#194721]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/InitTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import ru.job4j.tracker.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class InitTest {
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(new String[] {"0"});
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] {action});
+        assertThat(action.isCall(), is(true));
+    }
+    @Test
+    public void whenPrtMenu() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Menu.")
+                .add("0. Stub action")
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+}
