ea3838757f410b36fc89354967505415c9afdc9c
Kirill Romanov <kirill.coldline@gmail.com>
2019-12-10 14:12:16 +0300

    1. Singleton.[#194743]

---
+++ chapter_002/src/main/java/ru/job4j/pattern/singletone/TrackerSingle1.java
@@ -0,0 +1,77 @@
+package ru.job4j.pattern.singletone;
+
+import ru.job4j.tracker.Item;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public enum TrackerSingle1 {
+    INSTANCE;
+    private final Item[] items = new Item[100];
+    private int position = 0;
+
+    public Item add(Item item) {
+        return item;
+    }
+
+    public boolean replace(String id, Item item) {
+        boolean result = false;
+        for (int i = 0; i < this.position; i++) {
+            if (items[i].getId().equals(id)) {
+                items[i] = item;
+                result = true;
+                item.setId(id);
+                break;
+            }
+        }
+        return result;
+    }
+
+    public boolean delete(String id) {
+        boolean result = false;
+        for (int i = 0; i < this.position; i++) {
+            if (this.items[i].getId().equals(id)) {
+                this.items[i] = null;
+                System.arraycopy(items, 1 + i, items, i, this.position);
+                this.position--;
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, position);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] tmp = new Item[this.position];
+        int count = 0;
+        for (int i = 0; i < this.position; i++) {
+            if (this.items[i].getName().equals(key)) {
+                tmp[count] = this.items[i];
+                count++;
+            }
+        }
+        Item[] result = Arrays.copyOf(tmp, count);
+        return result;
+    }
+
+    public Item findById(String id) {
+        Item it = null;
+        for (int i = 0; i < this.position; i++) {
+            it = items[i];
+            if (it.getId().equals(id)) {
+                it = items[i];
+                break;
+            }
+        }
+        return it;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/singletone/TrackerSingleTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.singletone;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+
+import ru.job4j.pattern.singletone.TrackerSingle1;
+import ru.job4j.pattern.singletone.TrackerSingle2;
+import static org.hamcrest.Matchers.is;
+
+import ru.job4j.pattern.singletone.TrackerSingle3;
+import ru.job4j.pattern.singletone.TrackerSingle4;
+import ru.job4j.tracker.Tracker;
+
+public class TrackerSingleTest {
+    @Test
+    public void whenCreateSingletoneEnum() {
+        TrackerSingle1 firstRequest = TrackerSingle1.INSTANCE;
+        TrackerSingle1 secondRequest = TrackerSingle1.INSTANCE;
+        assertThat(firstRequest == secondRequest, is(true));
+    }
+    @Test
+    public void whenCreateSingletoneSynchronizedAccessor() {
+        TrackerSingle2 firstRequest = TrackerSingle2.getInstance();
+        TrackerSingle2 secondRequest = TrackerSingle2.getInstance();
+        assertThat(firstRequest == secondRequest, is(true));
+    }
+    @Test
+    public void whenCreateSingletoneWithInitializedObject() {
+        TrackerSingle3 firstRequest = TrackerSingle3.getInstance();
+        TrackerSingle3 secondRequest = TrackerSingle3.getInstance();
+        assertThat(firstRequest == secondRequest, is(true));
+    }
+    @Test
+    public void whenCreateSingletoneWithInnerClass() {
+        TrackerSingle4 firstRequest = TrackerSingle4.getInstance();
+        TrackerSingle4 secondRequest = TrackerSingle4.getInstance();
+        assertThat(firstRequest == secondRequest, is(true));
+    }
+}
