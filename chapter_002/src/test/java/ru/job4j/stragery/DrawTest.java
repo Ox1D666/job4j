ff40ecaa0fddb083b0bfbd591c19215bd0b02939
Kirill Romanov <kirill.coldline@gmail.com>
2020-01-10 17:01:38 +0300

    checkstyle changes

--- chapter_002/src/test/java/ru/job4j/stragery/DrawTest.java
+++ chapter_002/src/test/java/ru/job4j/stragery/DrawTest.java
@@ -10,10 +10,10 @@ public class DrawTest {
     @Test
     public void whenDrawSquare() {
         Square sq = new Square();
-        assertThat (sq.draw(), is(new StringBuilder().append("+++\n").append("+ +\n").append("+++").toString()));
+        assertThat(sq.draw(), is(new StringBuilder().append("+++\n").append("+ +\n").append("+++").toString()));
     }
     @Test
     public void whenDrawTriangle() {
-        assertThat (new Triangle().draw(), is(new StringBuilder().append("  X  \n").append(" X X  \n").append("X X X").toString()));
+        assertThat(new Triangle().draw(), is(new StringBuilder().append("  X  \n").append(" X X  \n").append("X X X").toString()));
     }
 }
