e52c0c377a4369647bf24210c3f76139336a7733
Kirill Romanov <kirill.coldline@gmail.com>
2020-01-13 18:56:59 +0300

    3. Конвертация ArrayList в двухмерный массив [#10035]

---
+++ chapter_003/src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.collection;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@Ignore
+public class ConvertList2ArrayTest {
+    @Test
+    public void when7ElementsThen9One() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 4);
+        int[][] expect = {{1, 2, 3, 4} , {5, 6, 7, 0}};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void when7ElementsThen9Two() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 3);
+        int[][] expect = {{1, 2, 3}, {4, 5, 6}, {7, 0, 0}};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void when7ElementsThen9Three() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 2);
+        int[][] expect = {{1, 2}, {3, 4} , {5, 6}, {7, 0}};
+        assertThat(result, is(expect));
+    }
+}
