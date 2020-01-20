71e68624c581c3b728d0289f98525f827956f505
Kirill Romanov <kirill.coldline@gmail.com>
2020-01-20 22:56:48 +0300

    3. Банковские переводы. [#10038]

---
+++ chapter_003/src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,57 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+
+    }
+
+    public void addAccount(String passport, Account account) {
+        for (Map.Entry<User, List<Account>> element : users.entrySet()) {
+            if (element.getKey().getPassport().contains(passport)) {
+                if (!element.getValue().contains(account)) {
+                    element.getValue().add(account);
+                }
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        User user = null;
+        for (User element : users.keySet()) {
+            if (element.getPassport().equals(passport)) {
+                user = element;
+            }
+        }
+        return user;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        Account account = null;
+        for (Map.Entry<User, List<Account>> element : users.entrySet()) {
+            if (element.getKey().getPassport().contains(passport)) {
+                for (Account value : element.getValue()) {
+                    if (value.getRequisite().equals(requisite)) {
+                        account = value;
+                    }
+                }
+            }
+        }
+        return account;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite,
+                                 double amount) {
+        boolean rsl = false;
+
+        return rsl;
+    }
+}
