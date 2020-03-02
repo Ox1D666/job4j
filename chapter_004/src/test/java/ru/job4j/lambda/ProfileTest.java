2f2fc124d82e79845cb14b2c75bba67692418e74
Kirill Romanov <kirill.coldline@gmail.com>
2020-03-02 19:19:04 +0300

    2. Список адресов[#194576]

--- chapter_004/src/test/java/ru/job4j/lambda/ProfileTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/ProfileTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.lambda;
 
 import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,6 +10,13 @@ import java.util.List;
 public class ProfileTest {
     @Test
     public void collectAddress() {
-
+        List<Address> result = List.of(new Address("Msk", "Gagarina", 12, 1961),
+                new Address("Nsk", "Lenina", 8, 1917));
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Msk", "Gagarina", 12, 1961)),
+                new Profile(new Address("Nsk", "Lenina", 8, 1917)));
+        Profiles prof = new Profiles();
+        List<Address> expect = prof.collect(profiles);
+        assertThat(expect, is(result));
     }
 }
