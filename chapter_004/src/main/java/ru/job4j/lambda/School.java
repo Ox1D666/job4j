e35c448aa0587a323eaba63f1cea84e341a9302e
Kirill Romanov <kirill.coldline@gmail.com>
2020-03-02 18:24:38 +0300

    0. Stream API улучшения[#194581]

--- chapter_004/src/main/java/ru/job4j/lambda/School.java
+++ chapter_004/src/main/java/ru/job4j/lambda/School.java
@@ -4,12 +4,13 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream().filter(predict).collect(Collectors.toList());
     }
     public List<Student> levelOf(List<Student> students, int bound) {
-        return students.stream().sorted().takeWhile(x -> x.getScore() > bound).collect(Collectors.toList());
+        return students.stream().sorted().flatMap(Stream::ofNullable).takeWhile(x -> x.getScore() > bound).collect(Collectors.toList());
     }
 }
