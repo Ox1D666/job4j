d06f2ebf33e20e445d42cb59aeaa81682c637929
Kirill Romanov <kirill.coldline@gmail.com>
2020-03-05 19:19:10 +0300

    2.0. Встроенные функциональные интерфейсы. [#249209]

---
+++ chapter_004/src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -0,0 +1,46 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.*;
+
+public class SearchAtt {
+    public static List<Attachment> filterSize(List<Attachment> list) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (att.getSize() > 100) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+
+    public static List<Attachment> filterName(List<Attachment> list) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (att.getName().contains("bug")) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+
+    private static List<Attachment> filter(List<Attachment> list, Predicate<Attachment> predicate){
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if(predicate.test(att)) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        List<Attachment> rsl = List.of(
+                new Attachment("first", 90),
+                new Attachment("bug", 110),
+                new Attachment("third", 150));
+        System.out.println(filter(rsl, x -> x.getSize() > 100 && x.getName().equals("bug")).size());
+
+    }
+}
