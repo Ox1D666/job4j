7cc64e24637dff4593f5fa55de337ac9164bb121
Kirill Romanov <kirill.coldline@gmail.com>
2020-03-11 14:11:58 +0300

    1. Подсчет функции в диапазоне.[#194567]

--- chapter_004/src/test/java/ru/job4j/lambda/FunctionCountTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/FunctionCountTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -23,6 +24,9 @@ public class FunctionCountTest {
     }
     @Test
     public void whenLogFunctionThenLogResults() {
-
+        List<Double> input = FunctionCount.diapason(5, 8, Math::log);
+        List<Double> result = input.stream().map(x -> Math.floor(x * Math.pow(10, 3)) / Math.pow(10, 3)).collect(Collectors.toList());
+        List<Double> expected = Arrays.asList(1.609, 1.791, 1.945);
+        assertThat(result, is(expected));
     }
 }
