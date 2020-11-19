5ccd06386e7239d482ac69db6d04effc2b605118
Kirill Romanov <kirill.coldline@gmail.com>
2020-11-19 17:09:42 +0300

    added user input validation

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 public class StartUI {
     public static void main(String[] args) {
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput(new ConsoleInput());
         Tracker tracker = new Tracker();
         MenuTracker menuTracker = new MenuTracker(input, tracker);
         menuTracker.init();
